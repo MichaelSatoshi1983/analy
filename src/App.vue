1137e49df7dc7274c5676861e71a72c267e5d82c
satoshi1983@zohomail.com <michael@localhost.localdomain>
2025-07-17 20:08:38 +0800

    修改中文和cf统计的代码

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,21 @@
       <div class="main">
         <div class="logo">
           <img src="./assets/favicon.ico">
-          <span>Han Analytics</span>
+          <span>undefined's Blog User Analytics</span>
         </div>
-        <h2>简单优雅的Web分析</h2>
+        <h2>一个托管在 Cloudflare Pages 上的免费开源网站分析仪表板</h2>
       </div>
     </header>
     <main>
       <header>
         <Alert>
           <AlertDescription>
-            <p>· Han Analytics 是一个简单的网络分析跟踪器和仪表板，托管在被称为赛博菩萨的 Cloudflare 上,无成本稳定运行,每天可达10万次免费统计。</p>
-            <p>· 域名、服务器、数据库 通通都不用! 托管在 Cloudflare Pages 上即可快速部署网站分析仪表板。</p>
-            <p style="font-weight: bold;">· 开源地址: <a class="git-link" href="https://github.com/uxiaohan/HanAnalytics"
+            <p>· 这是一个简单的网络分析跟踪器和仪表板，托管在 Cloudflare 上,无成本稳定运行,每天可达10万次免费统计。</p>
+            <p style="font-weight: bold;">· 项目开源地址: <a class="git-link" href="https://github.com/uxiaohan/HanAnalytics"
                 target="_blank">Han-Analytics</a>
+            </p>
+            <p style="font-weight: bold;">· 博客主站点: <a class="git-link" href="https://undefined.today"
+                target="_blank">undefined's Blog</a>
             </p>
           </AlertDescription>
         </Alert>
@@ -33,7 +35,7 @@
                 </SelectTrigger>
                 <SelectContent>
                   <SelectGroup>
-                    <SelectLabel>Web Site</SelectLabel>
+                    <SelectLabel>网页列表</SelectLabel>
                     <SelectItem :value="i" v-for="i in siteList" :key="i">{{ i }}</SelectItem>
                   </SelectGroup>
                 </SelectContent>
@@ -47,7 +49,7 @@
                 </SelectTrigger>
                 <SelectContent>
                   <SelectGroup>
-                    <SelectLabel>Cycle Time</SelectLabel>
+                    <SelectLabel>时间周期</SelectLabel>
                     <SelectItem :value="i.value" v-for="i in timeList" :key="i.name">{{ i.name }}</SelectItem>
                   </SelectGroup>
                 </SelectContent>
@@ -57,7 +59,7 @@
           <div
             class="flex justify-end text-center md:text-right line-clamp-1 [&>.views-item]:flex [&>.views-item]:flex-col [&>.views-item]:items-center md:[&>.views-item]:items-end [&>.views-item]:gap-4 [&>.views-item>span]:text-sm [&>.views-item>p]:text-3xl [&>.views-item>p]:line-clamp-1 [&>.views-item>p]:w-full">
             <div class="views-item w-full overflow-hidden">
-              <span>Views</span>
+              <span>浏览量</span>
               <div class="space-y-2 w-[50%]" v-if="resData.visit.views === undefined">
                 <Skeleton class="h-4  w-[50%] ml-auto" />
                 <Skeleton class="h-4" />
@@ -65,7 +67,7 @@
               <p v-else>{{ resData.visit.views }}</p>
             </div>
             <div class="views-item w-full overflow-hidden">
-              <span>Visitors</span>
+              <span>访客</span>
               <div class="space-y-2 w-[50%]" v-if="resData.visit.visitor === undefined">
                 <Skeleton class="h-4  w-[50%] ml-auto" />
                 <Skeleton class="h-4" />
@@ -73,7 +75,7 @@
               <p v-else>{{ resData.visit.visitor }}</p>
             </div>
             <div class="views-item w-full overflow-hidden">
-              <span>Visits</span>
+              <span>访问量</span>
               <div class="space-y-2 w-[50%]" v-if="resData.visit.visit === undefined">
                 <Skeleton class="h-4  w-[50%] ml-auto" />
                 <Skeleton class="h-4" />
@@ -88,7 +90,7 @@
         <div class="pt-20 grid md:grid-cols-2 sm:grid-cols-1 gap-[16px]">
           <Card class="box-border flex flex-col w-full h-[460px] overflow-hidden">
             <CardHeader>
-              <CardTitle>Pages</CardTitle>
+              <CardTitle>网页</CardTitle>
             </CardHeader>
             <CardContent class="box-border pt-0 w-full h-full overflow-hidden">
               <ScrollArea class="box-border p-2 pt-0 h-full w-full pages-list" v-if="resData.path != undefined">
@@ -113,7 +115,7 @@
 
           <Card class="box-border flex flex-col w-full h-[460px] overflow-hidden">
             <CardHeader>
-              <CardTitle>Referrers</CardTitle>
+              <CardTitle>来源</CardTitle>
             </CardHeader>
             <CardContent class="box-border pt-0 w-full h-full overflow-hidden">
               <ScrollArea class="box-border p-2 pt-0 h-full w-full pages-list" v-if="resData.referrer != undefined">
@@ -143,7 +145,7 @@
         <div class="pt-6 grid xl:grid-cols-3 gap-[16px] md:grid-cols-2 sm:grid-cols-1">
           <Card class="box-border flex flex-col w-full h-[460px] overflow-hidden">
             <CardHeader>
-              <CardTitle>Browsers</CardTitle>
+              <CardTitle>浏览器</CardTitle>
             </CardHeader>
             <CardContent class="box-border pt-0 w-full h-full overflow-hidden">
               <ScrollArea class="box-border p-2 pt-0 h-full w-full pages-list" v-if="resData.soft != undefined">
@@ -168,7 +170,7 @@
 
           <Card class="box-border flex flex-col w-full h-[460px] overflow-hidden">
             <CardHeader>
-              <CardTitle>OS</CardTitle>
+              <CardTitle>操作系统</CardTitle>
             </CardHeader>
             <CardContent class="box-border pt-0 w-full h-full overflow-hidden">
               <ScrollArea class="box-border p-2 pt-0 h-full w-full pages-list" v-if="resData.os != undefined">
@@ -195,7 +197,7 @@
 
           <Card class="box-border flex flex-col w-full h-[460px] overflow-hidden">
             <CardHeader>
-              <CardTitle>Areas</CardTitle>
+              <CardTitle>地区</CardTitle>
             </CardHeader>
             <CardContent class="box-border pt-0 w-full h-full overflow-hidden">
               <ScrollArea class="box-border p-2 pt-0 h-full w-full pages-list" v-if="resData.area != undefined">
@@ -222,6 +224,47 @@
       </section>
     </main>
     <footer>
+<!--      cf统计-->
+
+      <script type="text/javascript"
+              src="https://cdn.staticfile.net/jquery/3.4.1/jquery.min.js"></script>
+      <script>
+        getCDNinfo = function () {
+          $.ajax({
+            url: "/cdn-cgi/trace",
+            success: function (data, status) {
+              let areas = "Antananarivo, Madagascar - (TNR);Cape Town, South Africa - (CPT);Casablanca, Morocco - (CMN);Dar Es Salaam, Tanzania - (DAR);Djibouti City, Djibouti - (JIB);Durban, South Africa - (DUR);Johannesburg, South Africa - (JNB);Kigali, Rwanda - (KGL);Lagos, Nigeria - (LOS);Luanda, Angola - (LAD);Maputo, MZ - (MPM);Mombasa, Kenya - (MBA);Port Louis, Mauritius - (MRU);Réunion, France - (RUN);Bangalore, India - (BLR);Bangkok, Thailand - (BKK);Bandar Seri Begawan, Brunei - (BWN);Cebu, Philippines - (CEB);Chengdu, China - (CTU);Chennai, India - (MAA);Chittagong, Bangladesh - (CGP);Chongqing, China - (CKG);Colombo, Sri Lanka - (CMB);Dhaka, Bangladesh - (DAC);Dongguan, China - (SZX);Foshan, China - (FUO);Fuzhou, China - (FOC);Guangzhou, China - (CAN);Hangzhou, China - (HGH);Hanoi, Vietnam - (HAN);Hengyang, China - (HNY);Ho Chi Minh City, Vietnam - (SGN);Hong Kong - (HKG);Hyderabad, India - (HYD);Islamabad, Pakistan - (ISB);Jakarta, Indonesia - (CGK);Jinan, China - (TNA);Karachi, Pakistan - (KHI);Kathmandu, Nepal - (KTM);Kolkata, India - (CCU);Kuala Lumpur, Malaysia - (KUL);Lahore, Pakistan - (LHE);Langfang, China - (NAY);Luoyang, China - (LYA);Macau - (MFM);Malé, Maldives - (MLE);Manila, Philippines - (MNL);Mumbai, India - (BOM);Nagpur, India - (NAG);Nanning, China - (NNG);New Delhi, India - (DEL);Osaka, Japan - (KIX);Phnom Penh, Cambodia - (PNH);Qingdao, China - (TAO);Seoul, South Korea - (ICN);Shanghai, China - (SHA);Shenyang, China - (SHE);Shijiazhuang, China - (SJW);Singapore, Singapore - (SIN);Suzhou, China - (SZV);Taipei - (TPE);Thimphu, Bhutan - (PBH);Tianjin, China - (TSN);Tokyo, Japan - (NRT);Ulaanbaatar, Mongolia - (ULN);Vientiane, Laos - (VTE);Wuhan, China - (WUH);Wuxi, China - (WUX);Xi'an, China - (XIY);Yerevan, Armenia - (EVN);Zhengzhou, China - (CGO);Zuzhou, China - (CSX);Amsterdam, Netherlands - (AMS);Athens, Greece - (ATH);Barcelona, Spain - (BCN);Belgrade, Serbia - (BEG);Berlin, Germany - (TXL);Brussels, Belgium - (BRU);Bucharest, Romania - (OTP);Budapest, Hungary - (BUD);Chișinău, Moldova - (KIV);Copenhagen, Denmark - (CPH);Cork, Ireland -  (ORK);Dublin, Ireland - (DUB);Düsseldorf, Germany - (DUS);Edinburgh, United Kingdom - (EDI);Frankfurt, Germany - (FRA);Geneva, Switzerland - (GVA);Gothenburg, Sweden - (GOT);Hamburg, Germany - (HAM);Helsinki, Finland - (HEL);Istanbul, Turkey - (IST);Kyiv, Ukraine - (KBP);Lisbon, Portugal - (LIS);London, United Kingdom - (LHR);Luxembourg City, Luxembourg - (LUX);Madrid, Spain - (MAD);Manchester, United Kingdom - (MAN);Marseille, France - (MRS);Milan, Italy - (MXP);Moscow, Russia - (DME);Munich, Germany - (MUC);Nicosia, Cyprus - (LCA);Oslo, Norway - (OSL);Paris, France - (CDG);Prague, Czech Republic - (PRG);Reykjavík, Iceland - (KEF);Riga, Latvia - (RIX);Rome, Italy - (FCO);Saint Petersburg, Russia - (LED);Sofia, Bulgaria - (SOF);Stockholm, Sweden - (ARN);Tallinn, Estonia - (TLL);Thessaloniki, Greece - (SKG);Vienna, Austria - (VIE);Vilnius, Lithuania - (VNO);Warsaw, Poland - (WAW);Zagreb, Croatia - (ZAG);Zürich, Switzerland - (ZRH);Arica, Chile - (ARI);Asunción, Paraguay - (ASU);Bogotá, Colombia - (BOG);Buenos Aires, Argentina - (EZE);Curitiba, Brazil - (CWB);Fortaleza, Brazil - (FOR);Guatemala City, Guatemala - (GUA);Lima, Peru - (LIM);Medellín, Colombia - (MDE);Panama City, Panama - (PTY);Porto Alegre, Brazil - (POA);Quito, Ecuador - (UIO);Rio de Janeiro, Brazil - (GIG);São Paulo, Brazil - (GRU);Santiago, Chile - (SCL);Willemstad, Curaçao - (CUR);St. George's, Grenada - (GND);Amman, Jordan - (AMM);Baghdad, Iraq - (BGW);Baku, Azerbaijan - (GYD);Beirut, Lebanon - (BEY);Doha, Qatar - (DOH);Dubai, United Arab Emirates - (DXB);Kuwait City, Kuwait - (KWI);Manama, Bahrain - (BAH);Muscat, Oman - (MCT);Ramallah - (ZDM);Riyadh, Saudi Arabia - (RUH);Tel Aviv, Israel - (TLV);Ashburn, VA, United States - (IAD);Atlanta, GA, United States - (ATL);Boston, MA, United States - (BOS);Buffalo, NY, United States - (BUF);Calgary, AB, Canada - (YYC);Charlotte, NC, United States - (CLT);Chicago, IL, United States - (ORD);Columbus, OH, United States - (CMH);Dallas, TX, United States - (DFW);Denver, CO, United States - (DEN);Detroit, MI, United States - (DTW);Honolulu, HI, United States - (HNL);Houston, TX, United States - (IAH);Indianapolis, IN, United States - (IND);Jacksonville, FL, United States - (JAX);Kansas City, MO, United States - (MCI);Las Vegas, NV, United States - (LAS);Los Angeles, CA, United States - (LAX);McAllen, TX, United States - (MFE);Memphis, TN, United States - (MEM);Mexico City, Mexico - (MEX);Miami, FL, United States - (MIA);Minneapolis, MN, United States - (MSP);Montgomery, AL, United States - (MGM);Montréal, QC, Canada - (YUL);Nashville, TN, United States - (BNA);Newark, NJ, United States - (EWR);Norfolk, VA, United States - (ORF);Omaha, NE, United States - (OMA);Philadelphia, United States - (PHL);Phoenix, AZ, United States - (PHX);Pittsburgh, PA, United States - (PIT);Port-Au-Prince, Haiti - (PAP);Portland, OR, United States - (PDX);Queretaro, MX, Mexico - (QRO);Richmond, Virginia - (RIC);Sacramento, CA, United States - (SMF);Salt Lake City, UT, United States - (SLC);San Diego, CA, United States - (SAN);San Jose, CA, United States - (SJC);Saskatoon, SK, Canada - (YXE);Seattle, WA, United States - (SEA);St. Louis, MO, United States - (STL);Tampa, FL, United States - (TPA);Toronto, ON, Canada - (YYZ);Vancouver, BC, Canada - (YVR);Tallahassee, FL, United States - (TLH);Winnipeg, MB, Canada - (YWG);Adelaide, SA, Australia - (ADL);Auckland, New Zealand - (AKL);Brisbane, QLD, Australia - (BNE);Melbourne, VIC, Australia - (MEL);Noumea, New caledonia - (NOU);Perth, WA, Australia - (PER);Sydney, NSW, Australia - (SYD)".split(";");
+              let area = data.split("colo=")[1].split("\n")[0];
+              let areaCode = data.split("colo=")[1].split("\n")[0];
+              let tlsVersion = data.split("tls=")[1].split("\n")[0];
+              let httpVersion = data.split("http=")[1].split("\n")[0];
+              let sniValue = data.split("sni=")[1].split("\n")[0];
+              let ipValue = data.split("ip=")[1].split("\n")[0];
+              let userAgent = data.split("uag=")[1].split("\n")[0];
+
+              for (var i = 0; i < areas.length; i++) {
+                if (areas[i].indexOf(area) != -1) {
+                  document.getElementById("cdn").innerHTML = areas[i];
+                  break;
+                }
+              }
+              document.getElementById("tls").innerHTML = tlsVersion;
+              document.getElementById("http").innerHTML = httpVersion;
+              document.getElementById("sni").innerHTML = sniValue;
+              document.getElementById("ip").innerHTML = ipValue;
+              document.getElementById("useragent").innerHTML = userAgent;
+
+            }
+          })
+        }
+        $(document).ready(function () {
+          getCDNinfo();
+        });
+      </script>
+
+
+      <p style="text-align: center;">Open Source changes everything!</p>
+
       <p><img src="./assets/svg/ing.svg"></p>
       <p>
         <a href="https://pages.cloudflare.com" target="_blank" rel="noopener noreferrer"><img
@@ -229,8 +272,28 @@
         <a href="https://www.cloudflare.com/zh-cn/application-services/products/cdn/" target="_blank"
           rel="noopener noreferrer"><img src="./assets/svg/cdn.svg"></a>
         <a href="https://vuejs.org" target="_blank" rel="noopener noreferrer"><img src="./assets/svg/web.svg"></a>
-        <a href="https://api.vvhan.com" target="_blank"><img src="./assets/svg/surppot.svg"></a>
       </p>
+
+
+      <p style="text-align: center;">
+        <script>
+          document.write("本次加载耗时: " +
+            (performance.getEntriesByType('navigation').reduce((a, b) => a + b.responseEnd - b.startTime, 0) +
+              performance.getEntriesByType('resource').reduce((a, b) => a + b.responseEnd - b.startTime, 0)).toFixed(0) +
+            "ms")
+        </script>
+        <p style="text-align: center;">当前 SNI 状态： <span id="sni">正在统计！或可能被浏览器防追踪拦截！</span></p>
+        <p style="text-align: center;">当前 TLS 版本： <span id="tls">正在统计！或可能被浏览器防追踪拦截！</span></p>
+        <p style="text-align: center;">当前 HTTP 版本： <span id="http">正在统计！或可能被浏览器防追踪拦截！</span></p>
+        <p style="text-align: center;">当前您的客户端 IP 是： <span id="ip">正在统计！或可能被浏览器防追踪拦截！</span></p>
+        <p style="text-align: center;">当前分配的 CDN 节点是: <span id="cdn">正在统计！或可能被浏览器防追踪拦截！</span></p>
+        <p style="text-align: center;">您的 UserAgent 信息是: <span id="useragent">正在统计！或可能被浏览器防追踪拦截！</span></p>
+
+      </p>
+
+
+
+
     </footer>
   </section>
   <div class="z-[999999999]">
